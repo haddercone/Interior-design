35bebe65401ad977eebbd0f52177d3d61b8e0480
Robin Singh <32171406+haddercone@users.noreply.github.com>
2020-07-24 14:02:34 +0530

    Add files via upload

---
+++ interior design/index.js
@@ -0,0 +1,10 @@
+function navToggle(){
+    var nav = document.querySelector(".navigation");
+    if(nav.style.display == "grid"){
+        nav.style.display = "none";
+    }
+    else{
+        nav.style.display = "grid";
+    }
+    
+}
